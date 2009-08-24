3bacdf9da7a1805f0de2cf4d12fdf77e951a8fba
bcmpinc <bcmpinc@users.sourceforge.net>
2009-08-24 19:08:30 +0200

    Modified metadata for imperion

--- imperion_time_line.user.js
+++ imperion_time_line.user.js
@@ -1,18 +1,12 @@
 // Travian Time Line Main file
 var metadata = <><![CDATA[
 // ==UserScript==
-// @name           Travian Time Line
-// @namespace      TravianTL
-// @version        0.37
-// @description    Adds a time line on the right of each page to show events that have happened or will happen soon. Also adds a few other minor functions. Like: custom sidebar; resources per minute; ally lines; add to the villages list; colored marketplace.
+// @name           Imperion Time Line
+// @namespace      ImperionTL
+// @version        I-0.01
+// @description    Adds a buildqueue.
  
-// @include        http://*.travian*.*/*.php*
-// @exclude        http://forum.travian*.*
-// @exclude        http://board.travian*.*
-// @exclude        http://shop.travian*.*
-// @exclude        http://help.travian*.*
-// @exclude        http://*.travian*.*/manual.php*
-// @exclude        http://*.travian*.*/logout.php*
+// @include        http://*.imperion.org/*
  
 // @copyright      2008, 2009 Bauke Conijn, Adriaan Tichler (http://github.com/bcmpinc/travian-timeline-script)
 // @author         bcmpinc
@@ -51,18 +45,5 @@ var metadata = <><![CDATA[
  * <http://www.gnu.org.licenses/>
  *****************************************************************************/
   
-// This script improves the information provided by Travian. For example: by adding
-// a timeline that shows different events like completion of build tasks and the
-// arrival of armies. It does this by modify the html of the page.
-//
-// This script is completely passive, so it does not click links automatically or
-// send http requests. This means that for certain data to be collected you have to
-// read your reports and watch your ally page and allies profiles.
-//
-// This script can be combined with other scripts:
-// - If you have the 'Travian Task Queue'-script, you can click on the timeline to
-// automatically enter the schedule time.
-// - If you have the 'Travian Beyond'-script, additional villages will also get an
-// attack and a merchant link button. (Currently you have to add these additional
-// villages in the scripts source code.)
+// This script improves does a few things to ease playing Imperion.
 /*****************************************************************************/   
