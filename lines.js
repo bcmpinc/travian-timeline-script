ec91d0993f611da4ada2b432f1d7ee51008538d6
bcmpinc <bcmpinc@users.sourceforge.net>
2009-09-01 02:27:48 +0200

    Fixed problem with zooming out to wrong location on map

--- lines.js
+++ lines.js
@@ -16,15 +16,18 @@
 
 /****************************************
  * LINES (and circles)
+ * [actually just MAP]
  ****************************************/
 
-Feature.create("Lines",new Error().lineNumber-21);
+Feature.create("Map",new Error().lineNumber-21);
 
-Lines.s.enabled.description="Enable map enhacements";
-Lines.init=function(){
-    Lines.setting("hide_buttons", true, Settings.type.boolean,undefined, "Hide the movemente buttons and sector numbers on the system map.");
-    Lines.setting("scale", .05, Settings.type.integer,undefined, "The square at the start of a line will be at (this_value*location's_distance_from_center) from the center.");
-    Lines.setting("categories", { /* <tag>: [ <color> , <drawline> ], */
+Map.s.enabled.description="Enable map enhacements";
+Map.init=function(){
+    Map.setting("remove_nav_pad", true, Settings.type.boolean,undefined, "Remove the movemente joypad.");
+    Map.setting("remove_border_buttons", true, Settings.type.boolean,undefined, "Remove buttons at the border of the map.");
+    Map.setting("remove_sectors", true, Settings.type.boolean,undefined, "Remove the sector numbers at the top and right border of the map. Note that this numbering is not updated when dragging.");
+    Map.setting("scale", .05, Settings.type.integer,undefined, "The square at the start of a line will be at (this_value*location's_distance_from_center) from the center.");
+    Map.setting("categories", { /* <tag>: [ <color> , <drawline> ], */
             none: ["",false], // ie. remove from 'locations'.
                 owned: ["rgba(192,128,0,1.0)", true],
                 ally: ["rgba(0,0,255,0.5)", true],
@@ -36,13 +39,13 @@ Lines.init=function(){
                 ban: ["rgba(0,0,0,0.5)", false],
                 other: ["rgba(255,0,255,0.5)", true]
                 }, Settings.type.object, undefined, "The different types of categories. The order of this list defines the order in which they are listed and drawn.");
-    Lines.setting("locations", {}, Settings.type.object, undefined, "List of special locations.");
+    Map.setting("locations", {}, Settings.type.object, undefined, "List of special locations.");
     // A location is of the form [x,y,category,(name)]. Example: [-85,149,"ally"] or [12,-3,"extra","WW 1"]
     // name is optional.
 };
 
 // Creates the canvas for drawing the lines.
-Lines.create_canvas=function(x){
+Map.create_canvas=function(x){
     var pos = [x.offsetLeft, x.offsetTop, x.offsetWidth, x.offsetHeight];
 
     var canvas=document.createElement("canvas");
@@ -56,13 +59,13 @@ Lines.create_canvas=function(x){
     x.parentNode.insertBefore(canvas, x.nextSibling);
 
     var g = canvas.getContext("2d");
-    Lines.context = g;
-    Lines.pos = pos;
+    Map.context = g;
+    Map.pos = pos;
 };
 // Draws a line to the specified location
-Lines.touch=function(location) {
-    var x = location[0]-Lines.posx;
-    var y = location[1]-Lines.posy;
+Map.touch=function(location) {
+    var x = location[0]-Map.posx;
+    var y = location[1]-Map.posy;
     if (x<-400) x+=800;
     if (x> 400) x-=800;
     if (y<-400) y+=800;
@@ -72,14 +75,14 @@ Lines.touch=function(location) {
     px += py/50;
 
     // Get the location's category
-    var category=Lines.categories[location[2]];
+    var category=Map.categories[location[2]];
     // Get the drawing context
-    var g = Lines.context;
+    var g = Map.context;
     g.strokeStyle=category[0];
     if (category[1]) { // Draw lines only if enabled for category.
         g.beginPath();
-        var px2 = px * Lines.scale;
-        var py2 = py * Lines.scale;
+        var px2 = px * Map.scale;
+        var py2 = py * Map.scale;
         g.moveTo(px2,py2);
         g.lineTo(px,py);
         g.stroke();
@@ -99,150 +102,158 @@ Lines.touch=function(location) {
             g.lineWidth = 1;
     }
 };
-Lines.delayed_update=function() {
-    setTimeout(Lines.update,10);
+Map.delayed_update=function() {
+    setTimeout(Map.update,10);
 }
-Lines.update=function() {
+Map.update=function() {
     // But don't do an update when it's not necessary.
     try {
         z = unsafeWindow.m_c.z;
         if (z == null) return;
-        if (Lines.posx == z.x && Lines.posy == z.y) return;
-        Lines.posx = z.x - 0;
-        Lines.posy = z.y - 0;
+        if (Map.posx == z.x && Map.posy == z.y) return;
+        Map.posx = z.x - 0;
+        Map.posy = z.y - 0;
     } catch (e) {
-        this.exception("Lines.update", e);
+        this.exception("Map.update", e);
     }
     // Make sure the locations variable is up to date
-    Lines.s.locations.read();
+    Map.s.locations.read();
 
     // Get the drawing context
-    var g = Lines.context;
+    var g = Map.context;
 
     // Clear map
-    g.clearRect(0,0,Lines.pos[2],Lines.pos[3]);
+    g.clearRect(0,0,Map.pos[2],Map.pos[3]);
     g.save()
 
     // Initialize render context
-    g.translate(Lines.pos[2]/2-1,Lines.pos[3]/2 + 5.5);
+    g.translate(Map.pos[2]/2-1,Map.pos[3]/2 + 5.5);
     g.fillStyle = "rgba(128,128,128,0.8)";
 
     // Draw lines
-    for (var l in Lines.locations) {
-        Lines.touch(Lines.locations[l]);
+    for (var l in Map.locations) {
+        Map.touch(Map.locations[l]);
     }
 
     // Reset render context
     g.restore();
 }
 // The event listener (used by tag_tool)
-Lines.tag_change=function(e) {
-    Lines.s.locations.read();
+Map.tag_change=function(e) {
+    Map.s.locations.read();
     var cat = e.target.value;
-    var l = Lines.posx+","+Lines.posy;
+    var l = Map.posx+","+Map.posy;
     if (cat=="none") {
-        delete Lines.locations[l];
+        delete Map.locations[l];
     } else {
-        Lines.locations[l]=[Lines.posx,Lines.posy,cat,Lines.village_name];
+        Map.locations[l]=[Map.posx,Map.posy,cat,Map.village_name];
     }
-    Lines.s.locations.write();
+    Map.s.locations.write();
 };
 // add a "this location is special!" button to the map's village view. (if applicable)
-Lines.tag_tool=function() {
+Map.tag_tool=function() {
     if (location.href.indexOf("karte.php?d=")<=0) return;
     var x = document.evaluate( "//div[@id='content']/h1", document, null, XPathResult.ANY_UNORDERED_NODE_TYPE, null ).singleNodeValue;
     if (!x) return;
     var loc = x.textContent.match("\\((-?\\d+)\\|(-?\\d+)\\)");
-    var cat=Lines.locations[loc[1]+","+loc[2]];
+    var cat=Map.locations[loc[1]+","+loc[2]];
     cat=(cat==undefined)?cat="none":cat[2];
 
     var select=document.createElement("select");
-    for (var c in Lines.categories) {
+    for (var c in Map.categories) {
         var opt=document.createElement("option");
         opt.value=c;
         if (c==cat) opt.selected=true;
         opt.innerHTML=c;
         select.appendChild(opt);
     }
-    Lines.posx=loc[1]-0;
-    Lines.posy=loc[2]-0;
-    Lines.village_name=x.firstChild.textContent;
-    select.addEventListener('change',Lines.tag_change,false);
+    Map.posx=loc[1]-0;
+    Map.posy=loc[2]-0;
+    Map.village_name=x.firstChild.textContent;
+    select.addEventListener('change',Map.tag_change,false);
     x.appendChild(select);
     x.parentNode.style.zIndex=5; // Otherwise it might end up under the "(Capital)" text element.
 };
-Lines.next_move=0;
-Lines.mousemove=function(e) {
+Map.next_move=0;
+Map.mouse_distance=0;
+Map.mousedown=function(e) {
+  if ($("#mapSystem").css("visibility")=="visible") return; // Don't allow dragging when zoomed in to a solarsystem
+  Map.start_x=e.screenX;
+  Map.start_y=e.screenY;
+  Map.mouse_distance=0;
+  $("body").get(0).addEventListener("mousemove",Map.mousemove,false); // jquery's unbind did not work.
+};
+Map.mousemove=function(e) {
   var t=new Date().getTime();
-  if (t<Lines.next_move) return;
-  Lines.next_move=t+50; // mousemove events that happen whithin 50 ms of a previous one are dropped, to increase performance.
-  var dx = -(e.screenX-Lines.start_x);
-  var dy = -(e.screenY-Lines.start_y);
-  var map = $(Lines.unsafeMap.output)
+  if (t<Map.next_move) return;
+  Map.mouse_distance+=Math.sqrt((e.screenX-Map.start_x)^2+(e.screenY-Map.start_y)^2);
+  if (Map.mouse_distance<15) return; // Ignore small movements.
+  Map.next_move=t+50; // mousemove events that happen whithin 50 ms of a previous one are dropped, to increase performance.
+  var dx = -(e.screenX-Map.start_x);
+  var dy = -(e.screenY-Map.start_y);
+  var map = $(Map.unsafeMap.output)
   var pos = map.position();
   pos.left-=dx;
   pos.top -=dy;
   map.css({left: (pos.left)+"px",
            top:  (pos.top )+"px"});
   
-  var ix=Math.round(pos.left/Lines.quadrantWidth );
-  var iy=Math.round(pos.top /Lines.quadrantHeight);
-  Lines.unsafeMap.center.x = Lines.center.x-ix;
-  Lines.unsafeMap.center.y = Lines.center.y-iy;
-  Lines.unsafeMap.positionLeft = ix*Lines.quadrantWidth;
-  Lines.unsafeMap.positionTop  = iy*Lines.quadrantHeight;
+  var ix=Math.round(pos.left/Map.quadrantWidth );
+  var iy=Math.round(pos.top /Map.quadrantHeight);
+  Map.unsafeMap.center.x = Map.center.x-ix;
+  Map.unsafeMap.center.y = Map.center.y-iy;
+  Map.unsafeMap.positionLeft = ix*Map.quadrantWidth;
+  Map.unsafeMap.positionTop  = iy*Map.quadrantHeight;
   
-  for (var i=0; i<Lines.starLayerCount; i++) {
-    var layer = Lines.unsafeMap.starLayers[i];
-    var factor= Math.pow(Lines.starBasis, layer.layerNr-(-1)); // '--' is used to ensure it's a number
+  for (var i=0; i<Map.starLayerCount; i++) {
+    var layer = Map.unsafeMap.starLayers[i];
+    var factor= Math.pow(Map.starBasis, layer.layerNr-(-1)); // '--' is used to ensure it's a number
     layer.positionLeft-=dx*factor; // same idea here
     layer.positionTop -=dy*factor; // same idea here
     $(layer.output).css({left: layer.positionLeft+"px",
                          top:  layer.positionTop +"px"});
     layer._setStarPositions(-dy*factor,-dx*factor);
   }
-  Lines.start_x=e.screenX;
-  Lines.start_y=e.screenY;
+  Map.start_x=e.screenX;
+  Map.start_y=e.screenY;
 };
-Lines.mousedown=function(e) {
-  Lines.start_x=e.screenX;
-  Lines.start_y=e.screenY;
-  $("body").get(0).addEventListener("mousemove",Lines.mousemove,false); // jquery's unbind did not work.
-};
-Lines.end_drag = function(e) {
-  if (Lines.start_x == undefined) return;
-  $("body").get(0).removeEventListener("mousemove",Lines.mousemove,false); // jquery's unbind did not work.
-  Lines.last_move=0; 
-  Lines.mousemove(e);
-  Lines.start_x=undefined;
-  Lines.start_y=undefined;
+Map.end_drag = function(e) {
+  if (Map.start_x == undefined) return;
+  $("body").get(0).removeEventListener("mousemove",Map.mousemove,false); // jquery's unbind did not work.
+  Map.last_move=0; 
+  Map.mousemove(e);
+  Map.start_x=undefined;
+  Map.start_y=undefined;
 };
 
-Lines.run=function() {
+Map.run=function() {
   
   var x = $("#mapContent");
   if (x.size()>0) { // If this page has a map ...
-    if (Lines.hide_buttons)
-      GM_addStyle("#gridX, #gridY, #gridCorner, #mapNaviSmall, #mapNaviBig {display: none !important;} #mapContent, #mapContent #mapGalaxy {cursor: move;} #mapContent img {cursor: pointer;} #mapContent * {cursor: normal};");
+    var style="#mapContent, #mapContent #mapGalaxy {cursor: move;} #mapContent img {cursor: pointer;} #mapContent * {cursor: normal;} ";
+    if (Map.remove_nav_pad)        style+="#mapNaviSmall {display: none !important;} ";
+    if (Map.remove_border_buttons) style+="#mapNaviBig {display: none !important;} ";
+    if (Map.remove_sectors)        style+="#gridX, #gridY, #gridCorner {display: none !important;} ";
 
-    // These come from imperion's 'config.js'
-    Lines.starBasis = 1.0 / (unsafeWindow.config.performance.starBasis-0); // this is intentionally 1.0 devided by the original value
-    Lines.starLayerCount = unsafeWindow.config.performance.starLayerCount-0; 
-    Lines.quadrantWidth  = unsafeWindow.config.display.quadrantWidth -0;
-    Lines.quadrantHeight = unsafeWindow.config.display.quadrantHeight-0;
-    Lines.unsafeMap = unsafeWindow.config.registry.currentObject; // This is supposed to be the central instance of imperion's Map class.
-    Lines.center={x: Lines.unsafeMap.center.x-0,
-                  y: Lines.unsafeMap.center.y-0};
+    GM_addStyle(style);
     
+    // These come from imperion's 'config.js'
+    Map.starBasis = 1.0 / (unsafeWindow.config.performance.starBasis-0); // this is intentionally 1.0 devided by the original value
+    Map.starLayerCount = unsafeWindow.config.performance.starLayerCount-0; 
+    Map.quadrantWidth  = unsafeWindow.config.display.quadrantWidth -0;
+    Map.quadrantHeight = unsafeWindow.config.display.quadrantHeight-0;
+    Map.unsafeMap = unsafeWindow.config.registry.currentObject; // This is supposed to be the central instance of imperion's Map class.
+    Map.center={x: Map.unsafeMap.center.x-0,
+                y: Map.unsafeMap.center.y-0};
 
     y=$("body");
-    y.mouseleave(Lines.end_drag);
-    y.mouseup(Lines.end_drag);
-    x.mousedown(Lines.mousedown);
+    y.mouseleave(Map.end_drag);
+    y.mouseup(Map.end_drag);
+    x.mousedown(Map.mousedown);
   }
 
-  //Lines.tag_tool();
+  //Map.tag_tool();
 };
 
-Lines.call('init', true);
-$(function(){Lines.call('run',true);});
+Map.call('init', true);
+$(function(){Map.call('run',true);});
