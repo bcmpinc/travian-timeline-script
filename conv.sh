d28a862e39ae962bf51c67fe096a997baa464952
bcmpinc <bcmpinc@users.sourceforge.net>
2009-10-13 18:35:28 +0200

    Added script for quick convert to one file

---
+++ conv.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -e
+grep -v "@require" travian_time_line.user.js > script.js
+list=$(grep "@require" travian_time_line.user.js | sed -e 's/.*\/\([-a-z]*\.js\)/\1/i')
+for i in $list; do
+    echo -e "\n// FILE $i" >> script.js
+    cat $i >> script.js
+done
+
