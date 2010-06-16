5204a2b9a62eba99d7944505e3cb5f125cd19860
bcmpinc <bcmpinc@users.sourceforge.net>
2010-06-16 19:26:56 +0200

    Repaired conv.sh, the script deployment script

--- conv.sh
+++ conv.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 grep -v "@require" travian_time_line.user.js > script.js
-list=$(grep "@require" travian_time_line.user.js | sed -e 's/.*\/\([-a-z]*\.js\)/\1/i')
+list=$(grep "@require" travian_time_line.user.js | sed -e 's/.*\s\([-a-z]*\.js\)/\1/i')
 for i in $list; do
     echo -e "\n// FILE $i" >> script.js
     cat $i >> script.js
