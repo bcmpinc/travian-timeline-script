607a8b7f1c8231484917d7c1adc50059f1669345
bcmpinc <bcmpinc@users.sourceforge.net>
2010-06-22 19:39:18 +0200

    Converted Settings.js to use jquery

--- settings.js
+++ settings.js
@@ -135,6 +135,7 @@ Settings.read=function(scope) {
             x=x-0;
             break;
 
+            case Settings.type.set:
             case Settings.type.object:
             x=eval(x);
             break;
@@ -178,6 +179,7 @@ Settings.write=function(scope) {
             GM_setValue(param, this.get());
             break;
 
+            case Settings.type.set:
             case Settings.type.object:
             GM_setValue(param, uneval(this.get()));
             break;
@@ -191,7 +193,7 @@ Settings.write=function(scope) {
     }
 };
 
-// Removed the value from the GM persistent storage database aka about:config
+// Removes the value from the GM persistent storage database aka about:config
 // Scope is used to remove this setting from a higher scope.
 // Use either read() or write() after a call to this function.
 Settings.remove=function(scope) {
@@ -211,132 +213,125 @@ Settings.remove=function(scope) {
     }
 };
 
-// Appends a DOM element to parent_element that can be used to modify this setting.
-Settings.config=function(parent_element) {
+// Returns a jQuery object that can be used to modify this setting.
+Settings.config=function() {
     try {
-        var s  = document.createElement("span"); // the setting config thing
-        var sc = document.createElement("span"); // the scope
-        var setting = this;
-        var settingsname = this.name.replace(/_/g," ").pad(22);
-        var hint="";
-
-        // Add tooltip with a description (if available)
-        if (this.description) {
-            s.title = this.description;
-            var h = this.description.match("\\(([-a-zA-Z0-9.,_ ]+)\\)$");
-            if (h)
-                hint = " "+h[1];
-        }
+        var s = $.new("span"); // the setting config thing
+        var sc = $.new("span"); // the scope
+        s.append(sc);
+        s.append(this.name.replace(/_/g," ").pad(22)+": ");
+        var setting=this;
         
-        sc.style.marginRight="8px";
+        sc.css("marginRight", "8px");
         if (this.scope<this.scopes.length) {
-            sc.innerHTML = this.scope;
+            sc.html(this.scope);
             var sv=GM_getValue(this.scopes[setting.scope+1]+'.'+this.fullname);
             if (sv===undefined) sv = this.def_val;
             sc.title=sv;
         
             if (this.scope<this.scopes.length-1) {
-                sc.addEventListener("click",function (e) {
+                sc.bind("click",function (e) {
                         setting.remove(setting.scope);
                         setting.write(setting.scope+1);
                         Settings.fill();
                     },false);
-                sc.style.cursor="pointer";
-                sc.style.color="red";
+                sc.css({cursor: "pointer", color: "red"});
             }
         } else {
-             sc.innerHTML = 'd' 
+             sc.html('d');
         }
 
 
         // Create the input element.
         switch (this.type) {
         case Settings.type.none: {
-            s.innerHTML = settingsname+": "+this.get()+hint+"\n";
+            s.append(this.get());
             break;
         }
 
         case Settings.type.string:
         case Settings.type.integer: {
-            {
-                var input = '<input value="'+this.get()+'"/>';
-                s.innerHTML = settingsname+": "+input+hint+"\n";
-            }
-            s.childNodes[1].addEventListener("change",function (e) {
+            var input = $.new("input");
+            input.val(this.get());
+            s.append(input);
+            input.change(function (e) {
                     var val=e.target.value;
                     if (setting.type==Settings.type.integer) {
                         if (val=="") val = setting.def_val;
                         else val-=0;
                     }
+                    input.attr({value: val});
                     setting.set(val);
                     setting.write();
-                    Settings.fill(); // Redraw everything, in case a eval condition has changed
-                },false);
+                });
             break;
         }
 
         case Settings.type.enumeration: {
-            {
-                var select='<select>';
-                var j = this.get();
-                for (var i in this.typedata) {
-                    select+='<option value="'+i+'"';
-                    if (i==j) select+='selected="" ';
-                    select+='>'+this.typedata[i]+'</option>';
-                }
-                select+='</select>';
-                s.innerHTML = settingsname+": "+select+hint+"\n";
+            var select=$.new("select");
+            var j = this.get();
+            for (var i in this.typedata) {
+                option=$.new("option").attr({value: i}).html(this.typedata[i]);
+                if (i==j) option.attr({selected: "selected"});
+                select.append(option);
             }
-            s.childNodes[1].addEventListener("change",function (e) {
+            s.append(select);
+            select.change(function (e) {
                     var val=e.target.value-0;
                     setting.set(val);
                     setting.write();
-                    Settings.fill(); // Redraw everything
-                },false);
+                });
             break;
         }
         
         case Settings.type.object: {
             // TODO: have some more info for this object in some special cases.
-            s.innerHTML = settingsname+": (Object)"+hint+"\n";
+            s.append("(Object)");
             break;
         }
         
         case Settings.type.bool: {
-            s.style.cursor = "pointer";
-            s.style.color = this.get()?'green':'red';
-            s.innerHTML = settingsname+": <u>"+this.get()+"</u>"+hint+"\n";
-            s.addEventListener("click",function (e) {
-                    var val=!setting.get();
-                    s.style.color = val?'green':'red';
-                    s.childNodes[1].innerHTML = val;
-                    setting.set(val);
+            var u=$.new("u").html(""+this.get());
+            s.append(u);
+            s.css({cursor: "pointer", 
+                   color: (this.get()?'green':'red')});
+            s.click(function (e) {
+                    setting.set(!setting.get());
                     setting.write();
-                    Settings.fill(); // Redraw everything
-                },false);
+                    s.replaceWith(setting.config());
+                });
+            break;
+        }
+        
+        case Settings.type.set: {
+            for (var i in this.typedata) {
+                var u=$.new("u").html(this.typedata[i]);
+                u.css({cursor: "pointer", 
+                      color: (this.get()[i]?'green':'red')});
+                u.attr("id",this.name+"."+i);
+                u.click(function (e) {
+                        setting.get()[e.target.id.match(/\.(\d+)/)[1]-0]^=true;
+                        setting.write();
+                        s.replaceWith(setting.config());
+                    });
+                s.append("[").append(u).append("]");
+            }
             break;
         }
         }
-        // Insert the element.
-        if (setting.parent_el){ // If we have an expressed parent element
-            if (setting.parent_el.type == 'table'){ // create the tr's and td's
-                var tr = document.createElement('tr');
-                var td = document.createElement('td');
-                td.appendChild(sc);
-                td.appendChild(s);
-                tr.appendChild(td);
-                setting.parent_el.el.appendChild(tr);
-            } else {
-                setting.parent_el.el.appendChild(sc);
-                setting.parent_el.el.appendChild(s);
-            }
-        } else {
-            // Default if we have no given parent
-            parent_element.appendChild(sc);
-            parent_element.appendChild(s); 
+
+        // Add tooltip with a description (if available)
+        if (this.description) {
+            s.attr({title: this.description});
+            var h = this.description.match("\\(([-a-zA-Z0-9.,_ ]+)\\)$");
+            if (h)
+                s.append(" "+h[1]);
         }
+        s.append("\n");
+
+        return s;
     } catch (e) {
-        GM_log(e);
+        Settings.debug(e);
     }
 };
 
@@ -410,122 +405,115 @@ Settings.run=function() {
 };
 
 Settings.show=function() {
-    var w = document.createElement("div");
-    w.style.position = "fixed";
-    w.style.zIndex   = "750";
-    w.style.left     = "0px";
-    w.style.top      = "0px";
-    w.style.right    = "0px";
-    w.style.bottom   = "0px";
-    w.style.background = "rgba(192,192,192,0.8)";
-    w.innerHTML = '<a style="position: absolute; left: 0px; right: 0px; top: 0px; bottom: 0px; cursor: pointer;">'+
-                  '<span style="position: absolute; right: 30px; top: 20px;">[x] Close</span></a>'+
-                  '<div style="position: absolute; left: 50%; top: 50%;">'+
-                  '<pre style="position: absolute; left: -300px; top: -250px; width: 600px; height: 400px;'+
-                  ' border: 3px solid #000; background: #fff; overflow: auto; padding: 8px;'+
-                  ' -moz-border-radius-topleft:12px; -moz-border-radius-topright:12px;">'+
-                  '</pre></div>';
-    document.body.appendChild(w);
+    var w = $.new("div");
+    w.css({position:   "fixed",
+           zIndex:     "30000", // imperion developers like to exagerate
+           left:       "0px",
+           top:        "0px",
+           right:      "0px",
+           bottom:     "0px",
+           background: "rgba(192,192,192,0.8)"});
+    w.html('<a style="position: absolute; left: 0px; right: 0px; top: 0px; bottom: 0px; cursor: pointer;">'+
+           '<span style="position: absolute; right: 30px; top: 20px;">[x] Close</span></a>'+
+           '<div style="position: absolute; left: 50%; top: 50%;">'+
+           '<pre style="position: absolute; left: -300px; top: -250px; width: 600px; height: 400px;'+
+           ' border: 3px solid #000; background: #fff; overflow: auto; padding: 8px;'+
+           ' -moz-border-radius-topleft:12px; -moz-border-radius-topright:12px;" id="settings_container">'+
+           '</pre></div>');
+    w.find("a").click(Settings.close);
     Settings.window = w;
     try {
-        var p = w.childNodes[1];
-        function add_el(type) {
-            var el=document.createElement(type);
-            p.appendChild(el);
-            return el;
-        }
+        var p = w.find("div");
 
         // First we need to create the tabs...
-        var txt = '<tbody>';
+        var tablebody = $.new('tbody');
         for (var n in Feature.list){
             var f = Feature.list[n];
+            
+            // Skip features without settings
             if (f.s == undefined || isempty(f.s)) continue;
 
-            txt += '<tr align="right"><td style="padding: 5px 2px; text-align: right; border: none; background: none;">'+
-                '<a href="#" style="-moz-border-radius-topleft:8px; -moz-border-radius-bottomleft:8px;'+
+            tablebody.append('<tr align="right"><td style="padding: 5px 2px; text-align: right; border: none; background: none;">'+
+                '<a href="javascript:" style="-moz-border-radius-topleft:8px; -moz-border-radius-bottomleft:8px;'+
                 'padding:1px 11px 2px; border: 2px solid #000; '+
                 (n==Settings.current_tab?'background: #fff; border-right: none;':'background: #ddd; border-right: 3px solid black;')+
                 ' color:black; outline: none; cursor:pointer;">'+
-                f.name + '</a></td></tr>';
+                f.name + '</a></td></tr>');
         }
-        txt += '</tbody>';
 
         // Then we need to create the tab bar, to switch between tabs
-        var tabbar = add_el('table');
-        tabbar.innerHTML = txt;
-        tabbar.style.position="absolute";
-        tabbar.style.width = "150px";
-        tabbar.style.left  = "-445px";
-        tabbar.style.top   = "-200px";
-        tabbar.style.border= "none";
-        tabbar.style.borderCollapse = "collapse";
-        tabbar.style.background= "none";
+        var tabbar = $.new('table');        
+        tabbar.append(tablebody);
+        tabbar.css({position: "absolute",
+                    width:    "150px",
+                    left:     "-445px",
+                    top:      "-200px",
+                    border:   "none",
+                    borderCollapse: "collapse",
+                    background: "none"});
+        p.append(tabbar);
         
-        Settings.fill();
-        
-        var notice = add_el('pre'); // Add the copyright
-        notice.innerHTML="Copyright (C) 2008, 2009, 2010 Bauke Conijn, Adriaan Tichler\n"+
+        var notice = $.new('pre'); // Add the copyright
+        notice.text("Copyright (C) 2008, 2009, 2010 Bauke Conijn, Adriaan Tichler\n"+
             "GNU General Public License as published by the Free Software Foundation;\n"+
             "either version 3 of the License, or (at your option) any later version.\n"+
-            "This program comes with ABSOLUTELY NO WARRANTY!";
-        notice.style.color="#666";
-        notice.style.fontStyle="italic";
-        notice.style.fontSize="75%";
-        notice.style.textAlign="center";
-        notice.style.position="absolute";
-        notice.style.left="-300px";
-        notice.style.top="180px";
-        notice.style.width="600px";
-        notice.style.padding="1px 8px";
-        notice.style.border="3px solid #000";
-        notice.style.background="#fff";
-        notice.style.MozBorderRadiusBottomleft ="12px";
-        notice.style.MozBorderRadiusBottomright="12px";
+            "This program comes with ABSOLUTELY NO WARRANTY!");
+        notice.css({color: "#666",
+                    fontStyle: "italic",
+                    fontSize: "75%",
+                    textAlign: "center",
+                    position: "absolute",
+                    left: "-300px",
+                    top: "180px",
+                    width: "600px",
+                    padding: "1px 8px",
+                    border: "3px solid #000",
+                    background: "#fff",
+                    MozBorderRadiusBottomleft : "12px",
+                    MozBorderRadiusBottomright: "12px"});
+        p.append(notice);
 
         // Add click listeners to all of the tab buttons
-        var tabs=tabbar.childNodes[0].childNodes;
-        for (var n in tabs){
-            var a = tabs[n].childNodes[0].childNodes[0];
-            a.addEventListener('click', function(e){
-                var el = e.target;
-                var f = Feature.list[el.textContent];
-                Settings.current_tab=el.textContent;
-                Settings.s.current_tab.write();
+        var tabs=tabbar.find("a");
+        tabs.click(function(e){
+            var el = $(e.target);
+            var f = Feature.list[el.text()];
+            Settings.current_tab=el.text();
+            Settings.s.current_tab.write();
 
-                // Reset the background colours of *all* tab buttons
-                for (var i in tabs){
-                    tabs[i].childNodes[0].childNodes[0].style.background = "#ddd";
-                    tabs[i].childNodes[0].childNodes[0].style.borderRight = "3px solid black";
-                }
+            // Reset the background colours of *all* tab buttons
+            tabs.css({background: "#ddd",
+                      borderRight: "3px solid black"});
 
-                el.style.background = "#fff"; // Turn the colour of the clicked element white
-                el.style.borderRight = "none"; // Simulate that the tab is connected to the settings page
-
-                Settings.fill();
-            }, false);
-        }
+            el.css({background: "#fff", // Turn the colour of the clicked element white
+                    borderRight: "none"}); // Simulate that the tab is connected to the settings page
+            Settings.fill();
+        });
+        Settings.fill();
     } catch (e) {
-        this.exception("Settings.show", e);
+        if (this&&this.exception)
+            this.exception("Settings.show", e);
+        else
+            GM_log("FATAL:"+e);
     }
-    w.firstChild.addEventListener("click",Settings.close,false);
+    $("body").append(w);
 };
 
 // This fills/refreshes the display portion of the settings table
 Settings.fill=function(){
-    var disp = Settings.window.childNodes[1].childNodes[0];
+    var disp = Settings.window.find("#settings_container");
     var f = Feature.list[Settings.current_tab];
     if (f){
-        disp.innerHTML = '';
+        disp.empty();
         for (var i in f.s){ // And refill it
-            if (eval(f.s[i].hidden)) continue; // Ignore hidden elements
             f.s[i].read();
-            f.s[i].config(disp);
+            disp.append(f.s[i].config());
         }
     }
 }
 
 Settings.close=function(){
-    remove(Settings.window);
+    Settings.window.remove();
 };
 
 // Tested: this is *not* the cause of the pageload errors. Errors still occur without it running.
