ed5629f1623f113c7f681c23abc65ba277403ca6
bcmpinc <bcmpinc@users.sourceforge.net>
2010-06-23 13:05:46 +0200

    Fixed license url

--- imperion_time_line.user.js
+++ imperion_time_line.user.js
@@ -47,7 +47,7 @@ var metadata = <><![CDATA[
  * FOR A PARTICULAR PURPOSE. See the GNU Public License for more details
  *
  * To obtain a copy of the GNU General Public License, please see
- * <http://www.gnu.org.licenses/>
+ * <http://www.gnu.org/licenses/>
  *****************************************************************************/
   
 // This script improves a few things to ease playing Imperion.
